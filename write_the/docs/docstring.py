54f6554a745a06191bf90e0c91550dca6637fa70
Wytamma Wirth <wytamma.wirth@me.com>
2023-04-16 13:45:14 +1000

    :bug: move to cst to keep comments

--- write_the/docs/docstring.py
+++ write_the/docs/docstring.py
@@ -1,308 +1,135 @@
-import ast
+import libcst as cst
 
 
-def has_docstring(node):
-    """
-    Checks if a node has a docstring.
-    Args:
-      node (ast.AST): The node to check.
-    Returns:
-      bool: True if the node has a docstring, False otherwise.
-    """
-    if not node.body:
-        return False
-    first_stmt = node.body[0]
-    if not isinstance(first_stmt, ast.Expr):
-        return False
-    if not isinstance(first_stmt.value, ast.Constant):
-        return False
-    if first_stmt.value.kind not in (None, "u"):
-        return False
-    return True
-
-
-class DocstringAdder(ast.NodeTransformer):
+class DocstringAdder(cst.CSTTransformer):
     def __init__(self, docstrings, force):
-        """
-        Initializes a DocstringAdder object.
-        Args:
-          docstrings (dict): A dictionary of docstrings to add.
-          force (bool): Whether to force adding docstrings.
-        Attributes:
-          docstrings (dict): A dictionary of docstrings to add.
-          current_class (str): The current class name.
-          force (bool): Whether to force adding docstrings.
-        """
         self.docstrings = docstrings
         self.current_class = None
         self.force = force
 
-    def visit_FunctionDef(self, node):
-        """
-        Visits a function definition node and adds a docstring if necessary.
-        Args:
-          node (ast.FunctionDef): The node to visit.
-        Returns:
-          ast.FunctionDef: The visited node.
-        """
-        self.add_docstring(node)
-        return node
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef) -> cst.FunctionDef:
+        return self.add_docstring(updated_node)
 
-    def visit_ClassDef(self, node):
-        """
-        Visits a class definition node and adds a docstring if necessary.
-        Args:
-          node (ast.ClassDef): The node to visit.
-        Returns:
-          ast.ClassDef: The visited node.
-        """
-        self.current_class = node.name
-        self.add_docstring(node)
-        self.generic_visit(node)
+    def leave_ClassDef(self, original_node: cst.ClassDef, updated_node: cst.ClassDef) -> cst.ClassDef:
+        self.current_class = original_node.name.value
+        updated_node = self.add_docstring(updated_node)
         self.current_class = None
-        return node
+        return updated_node
 
     def add_docstring(self, node):
-        """
-        Adds a docstring to a node if necessary.
-        Args:
-          node (ast.AST): The node to add a docstring to.
-        """
-        key = f"{self.current_class}.{node.name}" if self.current_class else node.name
+        key = f"{self.current_class}.{node.name.value}" if self.current_class else node.name.value
         docstring = self.docstrings.get(key, None)
+
         if docstring and (not has_docstring(node) or self.force):
-            docstring_node = ast.Expr(value=ast.Constant(value=docstring, kind=None))
-            node.body.insert(0, docstring_node)
+            new_docstring = cst.parse_statement(f'"""{docstring}"""')
+            body = node.body.with_changes(body=[new_docstring] + list(node.body.body))
+            return node.with_changes(body=body)
+        return node
 
 
-class FunctionAndClassCollector(ast.NodeVisitor):
+class FunctionAndClassCollector(cst.CSTVisitor):
     def __init__(self, force):
-        """
-        Initializes a FunctionAndClassCollector object.
-        Args:
-          force (bool): Whether to force adding docstrings.
-        Attributes:
-          functions (list): A list of function names.
-          classes (list): A list of class names.
-          current_class (str): The current class name.
-          force (bool): Whether to force adding docstrings.
-        """
         self.functions = []
         self.classes = []
         self.current_class = None
         self.force = force
 
-    def visit_FunctionDef(self, node):
-        """
-        Visits a function definition node and adds the name to the list of functions if necessary.
-        Args:
-          node (ast.FunctionDef): The node to visit.
-        """
-        name = f"{self.current_class}.{node.name}" if self.current_class else node.name
+    def visit_FunctionDef(self, node: cst.FunctionDef):
+        name = f"{self.current_class}.{node.name.value}" if self.current_class else node.name.value
         if not has_docstring(node) or self.force:
             self.functions.append(name)
 
-    def visit_ClassDef(self, node):
-        """
-        Visits a class definition node and adds the name to the list of classes if necessary.
-        Args:
-          node (ast.ClassDef): The node to visit.
-        """
-        self.current_class = node.name
+    def visit_ClassDef(self, node: cst.ClassDef):
+        self.current_class = node.name.value
         if not has_docstring(node) or self.force:
-            self.classes.append(node.name)
-        self.generic_visit(node)
+            self.classes.append(node.name.value)
+        self.visit(node.body)
         self.current_class = None
 
-
-def get_node_names(ast, force):
-    """
-    Gets the names of nodes that need docstrings.
-    Args:
-      ast (ast.AST): The AST to search.
-      force (bool): Whether to force adding docstrings.
-    Returns:
-      list: A list of node names.
-    """
+def get_node_names(tree, force):
     collector = FunctionAndClassCollector(force)
-    collector.visit(ast)
+    tree.visit(collector)
     return collector.functions + collector.classes
 
-
-class DocstringRemover(ast.NodeTransformer):
+class DocstringRemover(cst.CSTTransformer):
     def __init__(self, nodes):
-        """
-        Initializes a DocstringRemover object.
-        Args:
-          nodes (list): A list of node names.
-        Attributes:
-          nodes (list): A list of node names.
-        """
         self.nodes = nodes
 
-    def visit_FunctionDef(self, node):
-        """
-        Visits a function definition node and removes the docstring if necessary.
-        Args:
-          node (ast.FunctionDef): The node to visit.
-        Returns:
-          ast.FunctionDef: The visited node.
-        """
-        if node.name in self.nodes:
-            self.remove_docstring(node)
-        return node
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef) -> cst.FunctionDef:
+        if original_node.name.value in self.nodes:
+            return self.remove_docstring(updated_node)
+        return updated_node
 
-    def visit_ClassDef(self, node):
-        """
-        Visits a class definition node and removes the docstring if necessary.
-        Args:
-          node (ast.ClassDef): The node to visit.
-        Returns:
-          ast.ClassDef: The visited node.
-        """
-        if node.name in self.nodes:
-            self.remove_docstring(node)
-        self.generic_visit(node)
-        return node
+    def leave_ClassDef(self, original_node: cst.ClassDef, updated_node: cst.ClassDef) -> cst.ClassDef:
+        if original_node.name.value in self.nodes:
+            return self.remove_docstring(updated_node)
+        return updated_node
 
     def remove_docstring(self, node):
-        """
-        Removes a docstring from a node if necessary.
-        Args:
-          node (ast.AST): The node to remove a docstring from.
-        """
-        if not node.body:
-            return
-        first_stmt = node.body[0]
-        if isinstance(first_stmt, ast.Expr) and isinstance(
-            first_stmt.value, ast.Constant
-        ):
-            node.body.pop(0)
+        if not node.body.body:
+            return node
+        first_stmt = node.body.body[0]
+        if isinstance(first_stmt, cst.SimpleStatementLine):
+            stmt = first_stmt.body[0]
+            if isinstance(stmt, cst.Expr) and isinstance(stmt.value, cst.SimpleString):
+                new_body = node.body.with_changes(body=node.body.body[1:])
+                return node.with_changes(body=new_body)
+        return node
 
-
-class NodeExtractor(ast.NodeVisitor):
+class NodeExtractor(cst.CSTVisitor):
     def __init__(self, nodes):
-        """
-        Initializes a NodeExtractor object.
-        Args:
-          nodes (list): A list of node names.
-        Attributes:
-          nodes (list): A list of node names.
-          extracted_nodes (list): A list of extracted nodes.
-        """
         self.nodes = nodes
         self.extracted_nodes = []
 
-    def visit_FunctionDef(self, node):
-        """
-        Visits a function definition node and adds it to the list of extracted nodes if necessary.
-        Args:
-          node (ast.FunctionDef): The node to visit.
-        """
-        if node.name in self.nodes:
+    def visit_FunctionDef(self, node: cst.FunctionDef):
+        if node.name.value in self.nodes:
             self.extracted_nodes.append(node)
-        self.generic_visit(node)
 
-    def visit_ClassDef(self, node):
-        """
-        Visits a class definition node and adds it to the list of extracted nodes if necessary.
-        Args:
-          node (ast.ClassDef): The node to visit.
-        """
-        if node.name in self.nodes:
+    def visit_ClassDef(self, node: cst.ClassDef):
+        if node.name.value in self.nodes:
             self.extracted_nodes.append(node)
-        self.generic_visit(node)
-
 
 def extract_nodes_from_tree(tree, nodes):
-    """
-    Extracts nodes from an AST.
-    Args:
-      tree (ast.AST): The AST to search.
-      nodes (list): A list of node names.
-    Returns:
-      list: A list of extracted nodes.
-    """
     extractor = NodeExtractor(nodes)
-    extractor.visit(tree)
+    tree.visit(extractor)
     return extractor.extracted_nodes
 
-
-class NodeRemover(ast.NodeTransformer):
+class NodeRemover(cst.CSTTransformer):
     def __init__(self, nodes):
-        """
-        Initializes a NodeRemover object.
-        Args:
-          nodes (list): A list of node names.
-        Attributes:
-          nodes (list): A list of node names.
-        """
         self.nodes = nodes
 
-    def visit_FunctionDef(self, node):
-        """
-        Visits a function definition node and removes it if necessary.
-        Args:
-          node (ast.FunctionDef): The node to visit.
-        Returns:
-          ast.FunctionDef: The visited node, or None if the node was removed.
-        """
-        if node.name in self.nodes:
-            return None
-        return node
+    def leave_FunctionDef(self, original_node: cst.FunctionDef, updated_node: cst.FunctionDef) -> cst.RemovalSentinel:
+        if original_node.name.value in self.nodes:
+            return cst.RemoveFromParent()
+        return updated_node
 
-    def visit_ClassDef(self, node):
-        """
-        Visits a class definition node and removes it if necessary.
-        Args:
-          node (ast.ClassDef): The node to visit.
-        Returns:
-          ast.ClassDef: The visited node, or None if the node was removed.
-        """
-        if node.name in self.nodes:
-            return None
-        self.generic_visit(node)
-        return node
+    def leave_ClassDef(self, original_node: cst.ClassDef, updated_node: cst.ClassDef) -> cst.RemovalSentinel:
+        if original_node.name.value in self.nodes:
+            return cst.RemoveFromParent()
+        return updated_node
 
+
+def has_docstring(node: cst.CSTNode) -> bool:
+    if isinstance(node, cst.FunctionDef) or isinstance(node, cst.ClassDef):
+        body = node.body.body
+        if body and isinstance(body[0], cst.SimpleStatementLine):
+            stmt = body[0].body[0]
+            if isinstance(stmt, cst.Expr) and isinstance(stmt.value, cst.SimpleString):
+                return True
+    return False
 
 def remove_nodes_from_tree(tree, nodes):
-    """
-    Removes nodes from an AST.
-    Args:
-      tree (ast.AST): The AST to search.
-      nodes (list): A list of node names.
-    Returns:
-      ast.AST: The modified AST.
-    """
     remover = NodeRemover(nodes)
-    tree = remover.visit(tree)
+    tree = tree.visit(remover)
     return tree
 
 
 def nodes_to_tree(nodes):
-    """
-    Converts a list of nodes to an AST.
-    Args:
-      nodes (list): A list of nodes.
-    Returns:
-      ast.AST: The generated AST.
-    """
-    module_node = ast.Module(body=[], type_ignores=[])
-    module_node.body.extend(nodes)
-    ast.fix_missing_locations(module_node)
-    return module_node
+    module = cst.Module(body=nodes)
+    return module
 
 
 def process_tree(tree, nodes, remove_nodes):
-    """
-    Processes an AST.
-    Args:
-      tree (ast.AST): The AST to process.
-      nodes (list): A list of node names.
-      remove_nodes (bool): Whether to remove nodes or extract them.
-    Returns:
-      ast.AST: The processed AST.
-    """
     if remove_nodes:
         return remove_nodes_from_tree(tree, nodes)
     else:
@@ -311,14 +138,6 @@ def process_tree(tree, nodes, remove_nodes):
 
 
 def remove_docstrings(tree, nodes):
-    """
-    Removes docstrings from an AST.
-    Args:
-      tree (ast.AST): The AST to process.
-      nodes (list): A list of node names.
-    Returns:
-      ast.AST: The processed AST.
-    """
     remover = DocstringRemover(nodes)
-    tree = remover.visit(tree)
+    tree = tree.visit(remover)
     return tree
--- write_the/docs/write.py
+++ write_the/docs/write.py
@@ -1,73 +1,45 @@
 from pathlib import Path
-import ast
+import libcst as cst
 from black import format_str, FileMode
-from .docstring import DocstringAdder, get_node_names, remove_docstrings, process_tree
+from .docstring import get_node_names, remove_docstrings, process_tree
+from write_the.cst import DocstringAdder
 from .chain import run
 
-
-def write_the_docs(
-    filename: Path, nodes=[], force=False, inplace=False, context=True
-) -> str:
-    '''
-    Writes docstrings to a given file.
-    Args:
-      filename (Path): The path to the file to write the docstrings to.
-      nodes (list): A list of nodes to write docstrings for.
-      force (bool): If True, overwrite existing docstrings.
-      inplace (bool): If True, write docstrings to the same file.
-      context (bool): If True, write docstrings for all nodes in the file.
-    Returns:
-      str: The modified source code with the docstrings added.
-    Notes:
-      If `nodes` is empty, docstrings will be written for all nodes in the file.
-      If `context` is False, docstrings will only be written for the nodes in `nodes`.
-    Examples:
-      >>> write_the_docs('example.py', nodes=['add', 'subtract'], force=True, inplace=True, context=False)
-      'def add(a, b):
-          """Adds two numbers.
-          Args:
-              a (int): The first number to add.
-              b (int): The second number to add.
-          Returns:
-              int: The sum of `a` and `b`.
-          """
-          return a + b
-      def subtract(a, b):
-          """Subtracts two numbers.
-          Args:
-              a (int): The first number to subtract.
-              b (int): The second number to subtract.
-          Returns:
-              int: The difference of `a` and `b`.
-          """
-          return a - b'
-    '''
+def write_the_docs(filename: Path, nodes=[], force=False, inplace=False, context=True) -> str:
     with open(filename, "r") as file:
         source_code = file.read()
+
     source_code = format_str(source_code, mode=FileMode())
-    tree = ast.parse(source_code)
+    tree = cst.parse_module(source_code)
     extract_specific_nodes = False
+
     if nodes:
         extract_specific_nodes = True
+        force = True
     else:
         nodes = get_node_names(tree, force)
-    processed_tree = remove_docstrings(tree, nodes)
+
+    remove_docstrings_tree = remove_docstrings(tree, nodes)
     if not context:
         if extract_specific_nodes:
-            processed_tree = process_tree(processed_tree, nodes, False)
+            processed_tree = process_tree(remove_docstrings_tree, nodes, False)
         else:
             all_nodes = get_node_names(tree, False)
             nodes_to_remove = [n for n in all_nodes if n not in nodes]
-            processed_tree = process_tree(processed_tree, nodes_to_remove, True)
-    code = ast.unparse(processed_tree)
+            processed_tree = process_tree(remove_docstrings_tree, nodes_to_remove, True)
+        code = processed_tree.code
+    else:
+        code = remove_docstrings_tree.code
     result = run(code=code, nodes=nodes)
     docstring_dict = {}
     for line in result.split("\n\n"):
         (node_name, docsting) = line.split(":", 1)
         docstring_dict[node_name] = docsting + "\n\n"
-    modified_tree = DocstringAdder(docstring_dict, force).visit(tree)
+    modified_tree = remove_docstrings_tree.visit(DocstringAdder(docstring_dict, force))
+
     if not inplace and extract_specific_nodes:
-        modified_tree = process_tree(tree, nodes, False)
-    modified_tree = ast.fix_missing_locations(modified_tree)
-    modified_code = ast.unparse(modified_tree)
+        modified_tree = process_tree(modified_tree, nodes, False)
+
+    modified_code = modified_tree.code
     return format_str(modified_code, mode=FileMode())
+
