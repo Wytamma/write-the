465fa6915e96505b299053a12df7cc2f2e64f990
Wytamma Wirth <wytamma.wirth@me.com>
2023-04-16 19:46:21 +1000

    :sparkles: write-the mkdocs

---
+++ write_the/cli/main.py
@@ -0,0 +1,92 @@
+import typer
+from write_the.__about__ import __version__
+from write_the.docs import write_the_docs
+from write_the.mkdocs import write_the_mkdocs
+from ..utils import list_python_files
+from pathlib import Path
+from rich.console import Console
+from rich.syntax import Syntax
+from typing import List, Optional
+
+app = typer.Typer()
+
+
+@app.callback()
+def callback():
+    """
+    Use GPT to code without having to copy-paste.
+    """
+
+
+@app.command()
+def docs(
+    file: Path = typer.Argument(..., help="Path to the file to generate docs."),
+    inplace: bool = typer.Option(
+        False, "--inplace", "-i", help="Replace the contents of the file."
+    ),
+    force: bool = typer.Option(
+        False, "--force", "-f", help="Generate docstings even if they already exist."
+    ),
+    context: bool = typer.Option(
+        False, "--context", "-c", help="Send context with nodes."
+    ),
+    nodes: List[str] = typer.Option(
+        None,
+        "--node",
+        "-n",
+        help="Generate docs for specific nodes (functions and classes).",
+    ),
+):
+    """
+    Document and format your code!
+    """
+    if file.is_dir():
+        files = list_python_files(file)
+    else:
+        assert file.suffix == ".py"
+        files = [file]
+    for file in files:
+        if len(files) > 1:
+            typer.secho(file, fg="green")
+        result = write_the_docs(
+            file, nodes=nodes, force=force, inplace=inplace, context=context
+        )
+        if inplace:
+            with open(file, "w") as f:
+                f.writelines(result)
+        else:
+            syntax = Syntax(result, "python")
+            console = Console()
+            console.print(syntax)
+
+
+@app.command()
+def mkdocs(
+    code_dir: Path = typer.Argument(
+        ...,
+        help="Path to the projects code. Will use docstings for reference.",
+        file_okay=False,
+    ),
+    readme: Optional[Path] = typer.Option(
+        None, help="Path to projects README (used to create index.md).", dir_okay=False
+    )
+):
+    """
+    Generates mkdocs for a project and populate the API reference
+    """
+    write_the_mkdocs(code_dir=code_dir, readme=readme)
+
+
+@app.command()
+def tests():
+    raise NotImplementedError()
+
+
+@app.command()
+def refactor():
+    raise NotImplementedError()
+
+
+@app.command()
+def optimise():
+    raise NotImplementedError()
---
+++ write_the/mkdocs/write.py
@@ -0,0 +1,78 @@
+import os
+from pathlib import Path
+from collections import defaultdict
+from write_the.utils import list_python_files
+
+
+def write_the_mkdocs(code_dir: Path, readme: Path = None, project_name=None):
+    """
+    Generates a mkdocs.yml file and reference files for a project.
+    Args:
+      readme (Path): Path to the project's README.
+      code_dir (Path): Path to the project's code directory.
+      project_name (str, optional): Name of the project. Defaults to the name of the current working directory.
+      force (bool, optional): If True, overwrite existing mkdocs.yml and index.md files. Defaults to False.
+    Notes:
+      If `project_name` is not provided, the name of the current working directory is used.
+      If `force` is False, existing mkdocs.yml and index.md files will not be overwritten.
+    Returns:
+      None
+    """
+    files = list_python_files(code_dir)
+    groups = [path.stem for path in code_dir.glob("*") if not path.stem.startswith("_")]
+
+    if not project_name:
+        project_name = Path(os.getcwd()).name
+    mkdocs = mkdocs_template.format(project_name=project_name)
+    references = defaultdict(list)
+    for file in files:
+        if file.name.startswith("_"):
+            continue
+        key = "index"
+        for group in groups:
+            if group in str(file):
+                key = group
+                break
+        module = str(file).rstrip(".py").replace("/", ".")  # breaks on windows?
+        references[key].append(f"::: {module}")
+
+    reference_path = Path("./docs/reference/")
+    reference_path.mkdir(parents=True, exist_ok=True)
+    for doc in references:
+        with open(f"{reference_path}/{doc}.md", "w") as f:
+            for ref in references[doc]:
+                f.write(ref + "\n\n")
+
+    if readme:
+        index_text = f"---\ntitle: Home\n---\n{readme.read_text()}"
+        Path("./docs/index.md").write_text(index_text)
+    if not Path("./mkdocs.yml").exists():
+        Path("./mkdocs.yml").write_text(mkdocs)
+
+
+mkdocs_template = """
+site_name: {project_name}
+# repo_url: https://github.com/wytamma/write-the
+
+theme:
+  name: "material"
+  # homepage: https://github.com/wytamma/write-the
+  # logo: assets/logo.png
+  # favicon: images/favicon.png
+  palette: 
+    - scheme: default
+      toggle:
+        icon: material/brightness-7 
+        name: Switch to dark mode
+    - scheme: slate
+      toggle:
+        icon: material/brightness-4
+        name: Switch to light mode
+  features:
+    - toc.follow
+    - content.action.edit
+
+plugins:
+- search
+- mkdocstrings
+"""
