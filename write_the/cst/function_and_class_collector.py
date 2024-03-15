2f57e0ce56ff3d3a62e4e058df94dcd3e2a537ea
Wytamma Wirth <wytamma.wirth@me.com>
2024-03-15 18:07:14 +1100

    :bug: only update nodes that already have docstrings

--- write_the/cst/function_and_class_collector.py
+++ write_the/cst/function_and_class_collector.py
@@ -3,20 +3,26 @@ from .utils import has_docstring
 
 
 class FunctionAndClassCollector(cst.CSTVisitor):
-    def __init__(self, force):
+    """
+    A CSTVisitor that collects the names of functions and classes from a CST tree.
+    """
+    def __init__(self, force, update):
         """
         Initializes the FunctionAndClassCollector.
+
         Args:
           force (bool): Whether to force the collection of functions and classes even if they have docstrings.
         """
         self.functions = []
         self.classes = []
         self.force = force
+        self.update = update
         self.current_class = None
 
     def visit_FunctionDef(self, node: cst.FunctionDef) -> None:
         """
-        Visits a FunctionDef node and adds it to the list of functions if it does not have a docstring or if `force` is `True`.
+        Visits a FunctionDef node and adds its name to the list of functions if it does not have a docstring or if `force` is `True`.
+
         Args:
           node (cst.FunctionDef): The FunctionDef node to visit.
         """
@@ -25,33 +31,47 @@ class FunctionAndClassCollector(cst.CSTVisitor):
             if self.current_class
             else node.name.value
         )
-        if not has_docstring(node) or self.force:
+        if self.force:
+            self.functions.append(name)
+        elif has_docstring(node) and self.update:
+            self.functions.append(name)
+        elif not has_docstring(node) and not self.update:
             self.functions.append(name)
 
     def visit_ClassDef(self, node: cst.ClassDef) -> None:
         """
-        Visits a ClassDef node and adds it to the list of classes if it does not have a docstring or if `force` is `True`.
+        Visits a ClassDef node and adds its name to the list of classes if it does not have a docstring or if `force` is `True`. Also sets the current class name for nested function collection.
+
         Args:
           node (cst.ClassDef): The ClassDef node to visit.
         """
         self.current_class = node.name.value
-        if not has_docstring(node) or self.force:
+        if self.force:
+            self.classes.append(node.name.value)
+        elif has_docstring(node) and self.update:
+            self.classes.append(node.name.value)
+        elif not has_docstring(node) and not self.update:
             self.classes.append(node.name.value)
         # self.visit_ClassDef(node)  # Call the superclass method to continue the visit
 
     def leave_ClassDef(self, node: cst.ClassDef) -> None:
+        """
+        Resets the current class name when leaving a ClassDef node.
+        """
         self.current_class = None
 
 
-def get_node_names(tree, force):
+def get_node_names(tree, force, update):
     """
     Gets the names of functions and classes from a CST tree.
+
     Args:
       tree (cst.CSTNode): The CST tree to traverse.
       force (bool): Whether to force the collection of functions and classes even if they have docstrings.
+
     Returns:
       list[str]: A list of function and class names.
     """
-    collector = FunctionAndClassCollector(force)
+    collector = FunctionAndClassCollector(force, update)
     tree.visit(collector)
     return collector.classes + collector.functions
